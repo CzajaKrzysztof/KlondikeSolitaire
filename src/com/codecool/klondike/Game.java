fd845036df1711383602b2c00a9b332baa94035b
Krzysiek <krzysiekczaja@gmail.com>
2019-04-04 09:16:16 +0200

    HotFix of releaseHanndler methode

--- src/com/codecool/klondike/Game.java
+++ src/com/codecool/klondike/Game.java
@@ -119,9 +119,6 @@ public class Game extends Pane {
 
         if (pile != null) {
             handleValidMove(card, pile);
-            if (isGameWon()){
-                gameWon(this.attachedStage);
-            }
         } else {
             draggedCards.forEach(MouseUtil::slideBack);
             draggedCards.clear();
@@ -264,7 +261,7 @@ public class Game extends Pane {
     }
 
     public void dealCards() {
-        Collections.shuffle(deck);
+        // Collections.shuffle(deck);
         Iterator<Card> deckIterator = deck.iterator();
         int cardsAmount = 1;
         for (Pile pile: tableauPiles) {
